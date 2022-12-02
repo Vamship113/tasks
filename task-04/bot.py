a6bbdaaffdd0fb4b0988cec7a3a6fedb6bcf0edd
Vamship113 <vinnup113@gmail.com>
2022-12-02 17:21:49 +0530

    finished task-04

--- task-04/bot.py
+++ task-04/bot.py
@@ -5,11 +5,12 @@ import json
 import csv
 
 # TODO: 1.1 Get your environment variables 
-yourkey = os.getenv()
-bot_id = os.getenv()
+yourkey = "60b64a7f"
+bot_id = "5724015164:AAEnlp-L2TJlTtLUe1k1O8pMzNhgcaxu1Xg"
 
 bot = telebot.TeleBot(bot_id)
-
+global final
+final=[]
 @bot.message_handler(commands=['start', 'hello'])
 def greet(message):
     global botRunning
@@ -21,6 +22,7 @@ def greet(message):
 def goodbye(message):
     global botRunning
     botRunning = False
+    os.remove("data.csv")
     bot.reply_to(message, 'Bye!\nHave a good time')
     
 
@@ -32,7 +34,25 @@ def helpProvider(message):
 
 @bot.message_handler(func=lambda message: botRunning, commands=['movie'])
 def getMovie(message):
-    bot.reply_to(message, 'Getting movie info...')
+    global final
+    tex=message.text
+    response = requests.get(f"http://www.omdbapi.com/?apikey=60b64a7f&t={tex[7:]}")
+    data=response.json()
+    l=[data['Title'],data['Year'],data['Released'],data['imdbRating']]
+    final.append(l)
+    final_test=final
+    bot.reply_to(message, 'Getting movie info..')
+    bot.send_message(message.chat.id,'Movie Found!')
+    s='Movie Name: ' +data['Title']+'\nYear: '+data['Year']+'\nReleased: '+data['Released']+'\nimdbRating: '+data['imdbRating']
+    bot.send_photo(message.chat.id,data['Poster'],s)
+    header=['Title','Year','Released','imdbRating']
+    with open('data.csv','w',encoding='UTF8',newline='') as f:
+       writer=csv.writer(f)
+      
+       writer.writerow(header)
+       writer.writerows(final)
+    
+   
     # TODO: 1.2 Get movie information from the API
     # TODO: 1.3 Show the movie information in the chat window
     # TODO: 2.1 Create a CSV file and dump the movie information in it
@@ -40,7 +60,11 @@ def getMovie(message):
   
 @bot.message_handler(func=lambda message: botRunning, commands=['export'])
 def getList(message):
-    bot.reply_to(message, 'Generating file...')
+    global final_test
+    bot.reply_to(message, 'Generating file...')    
+    bot.send_document(message.chat.id, open('data.csv', 'rb'))
+    
+    
     #TODO: 2.2 Send downlodable CSV file to telegram chat
 
 @bot.message_handler(func=lambda message: botRunning)
@@ -48,3 +72,4 @@ def default(message):
     bot.reply_to(message, 'I did not understand '+'\N{confused face}')
     
 bot.infinity_polling()
+
---
+++ task-04/os.py
@@ -0,0 +1,2 @@
+yourkey="60b64a7f"
+bot_id="5724015164:AAEnlp-L2TJlTtLUe1k1O8pMzNhgcaxu1Xg"
